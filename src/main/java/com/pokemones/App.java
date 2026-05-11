265b5bde03d31f8d091da718ec4235035ca46a1f
Carlinhos-87 <jcpapark87@gmail.com>
2026-05-11 17:28:20 +0200

    Creada rama nueva

--- src/main/java/com/pokemones/App.java
+++ src/main/java/com/pokemones/App.java
@@ -1,10 +1,7 @@
 package com.pokemones;
 
-/**
- * Hello world!
- */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hazte con todos!!!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hazte con todos!!!");
+	}
 }
